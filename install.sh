9e234504c9467493fc0d7cb84d81c4fc249d80b5
Memory-Studio <sb.nmd@outlook.com>
2024-05-12 13:16:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ sudo mv -i ~/.config/nvim ~/.config/nvim.bak
 echo -e "\e[35m2) Download init.lua\e[0m"
 sudo git clone https://mirror.ghproxy.com/github.com/lunarlands/NeoVim.git ~/.config/nvim
 echo -e "\e[35m3) Enjoy!\e[0m"
-cd ~/.config/nvim
-sudo rm -rf .git *.sh LICENSE
 sudo nvim
