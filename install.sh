8391aada091391792287f62712421460d075bfbc
Memory-Studio <sb.nmd@outlook.com>
2024-05-12 13:00:01 +0800

    R.O.Lunar&TZGML: Version 240512 - install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ echo "This script will run after 3 second. (Ctrl+C EXIT)"
 sleep 3
 echo "1) Clean & mkdir"
 sudo mv -i ~/.config/nvim ~/.config/nvim.bak
-sudo mkdir ~/.config/nvim
 echo "2) Download init.lua"
-sudo curl https://raw.githubusercontent.com/lunarlands/NeoVim/main/init.lua >> ~/.config/nvim/init.lua
+sudo git clone https://mirror.ghproxy.com/github.com/lunarlands/NeoVim.git ~/.config/nvim
 echo "3) Enjoy!"
-nvim
+sudo nvim
