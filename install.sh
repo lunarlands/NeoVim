e84c09658c1abc44cbadca7b9063973f73504d2b
Memory-Studio <sb.nmd@outlook.com>
2024-05-12 13:16:01 +0800

    Update Install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 echo "TZGML & Republic Of Lunar's NVIM | Installer"
-
-echo -e "\e[35m::\e[0m Are you sure about that? [Enter YES]"
-read confirm
-if [[ $confirm -ne "YES" ]]; then
-    exit
-fi
-
 echo -e "\e[35m1) Clean & mkdir\e[0m"
 sudo mv -i ~/.config/nvim ~/.config/nvim.bak
 echo -e "\e[35m2) Download init.lua\e[0m"
 sudo git clone https://mirror.ghproxy.com/github.com/lunarlands/NeoVim.git ~/.config/nvim
 echo -e "\e[35m3) Enjoy!\e[0m"
+cd ~/.config/nvim
+sudo rm -rf .git *.sh LICENSE
 sudo nvim
